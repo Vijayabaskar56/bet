ebfe1c5e8c7587f2c24a58d31754c8e4d9dde6a1
gajendran <gajegaja618@gmail.com>
2025-12-25 22:58:18 +0530

    feat:version update for the user panel in 21

---
+++ apps/user/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Home
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
--- apps/user/src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    base_url: 'http://localhost:3000',
-    socket_url: 'wss://localhost:3000',
-    // base_url: 'https://betting.yoursdemo.com/base/',
-    // socket_url: 'wss://betting.yoursdemo.com',
-    CLIENT_SECRET: "Sportsbetting@2025"
-};
