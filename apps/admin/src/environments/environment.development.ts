a8932c73b8b378a9bb5723fead9693be5bf52465
vijayabaskar <vj2k02@gmail.com>
2025-12-26 00:46:35 +0530

    core: merge remainings

---
+++ apps/admin/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    base_url:'https://bettingagent.yoursdemo.com/base/',
+    SOCKET_URL : 'https://bettingagent.yoursdemo.com/',
+    PAGE_SIZE:5,
+    CLIENT_SECRET : "Sportsbetting@2025"
+};
---
+++ apps/admin/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    // base_url:'http://192.168.0.54:9696/',
+    base_url : 'https://bettingagent.yoursdemo.com/base/',
+    SOCKET_URL : 'https://bettingagent.yoursdemo.com/',
+    PAGE_SIZE:5,
+    CLIENT_SECRET : "Sportsbetting@2025"
+};
